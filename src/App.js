1e4f5191aac67b9c748fc1509a0a794261696739
Elie Mbuyi Kayembe <mbuyieliek@gmail.com>
2023-07-23 20:52:13 +0200

    useform with the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useForm} from "react-hook-form";
 
 function App() {
+
+  const {handleSubmit, register,getValues,watch} = useForm();
+  
+  watch("name");
+
+  function submit(value){
+    console.log(value);
+  }
+
+  console.log(getValues());
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-row justify-content-center align-items-center"
+    style={{background : "#fefefe", height:"100vh", width:"100%"}}
+    >
+     <form onClick={handleSubmit(submit)}>
+      <div className="d-flex flex-column mb-20">
+        <label htmlFor="name">
+          Nom
+        </label>
+        <input {...register("name")} type="text" id="name" />
+      </div>
+      <button className="btn btn-primary">Save</button>
+     </form>
     </div>
   );
 }
 
 export default App;
+
+
+// handleSubmit : soumettre le formulaire
+//getValues : recuperer les valeurs 
+// register : enregistrer un input a l'interieur d'un formulaire
